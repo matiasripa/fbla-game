326283738ef401b77c11847773e2a5d7056a9352
Ahmad-fakhouri-1 <fafafa1972@gmail.com>
2024-11-26 17:12:11 -0800

    Update field.gd
    new

--- example/field/field.gd
+++ example/field/field.gd
@@ -1,20 +1,24 @@
 class_name Field
 extends MarginContainer
 
-
 @onready var card_drop_area_right: Area2D = $CardDropAreaRight
 @onready var card_drop_area_left: Area2D = $CardDropAreaLeft
 @onready var cards_holder: HBoxContainer = $CardsHolder
 @export var isasset: bool = false
-@export var iswithdraw:bool = false
+@export var iswithdraw: bool = false
 var cardResource = preload("res://example/card/card.tscn")
 
-#this is the card values,add more interesting values
-#["description of the card",amount of turns,money,iron,reputation,C02]
-var card_positive = [["good status1",1,2,3,4,5],["goodstatus2",1,2,3,4,5],["goodstatus2",1,2,3,4,5]]
-var card_negative = [["bad status1",1,2,3,4,5],["bad status2",1,2,3,4,5],["bad status2",1,2,3,4,5]]
-var positiverand = 0
-var negativerand = 0
+# Card values: [description of the card, amount of turns, money, iron, reputation, CO2]
+var card_positive = [
+	["good status1", 1, 2, 3, 4, 5],
+	["good status2", 1, 2, 3, 4, 5],
+	["good status3", 1, 2, 3, 4, 5]
+]
+var card_negative = [
+	["bad status1", 1, 2, 3, 4, 5],
+	["bad status2", 1, 2, 3, 4, 5],
+	["bad status3", 1, 2, 3, 4, 5]
+]
 
 func _ready():
 	$Label.text = name
@@ -23,16 +27,13 @@ func _ready():
 		var card := child as Card
 		card.home_field = self
 
-
 func return_card_starting_position(card: Card):
 	card.reparent(cards_holder)
 	cards_holder.move_child(card, card.index)
 
-
 func set_new_card(card: Card):
 	card_reposition(card)
 	card.home_field = self
-
 
 func card_reposition(card: Card):
 	var field_areas = card.drop_point_detector.get_overlapping_areas()
@@ -40,7 +41,6 @@ func card_reposition(card: Card):
 	var index: int = 0
 	
 	if cards_areas.is_empty():
-		print(field_areas.has(card_drop_area_left))
 		if field_areas.has(card_drop_area_right):
 			index = cards_holder.get_children().size()
 	elif cards_areas.size() == 1:
@@ -52,30 +52,28 @@ func card_reposition(card: Card):
 		index = cards_areas[0].get_parent().get_index()
 		if index > cards_areas[1].get_parent().get_index():
 			index = cards_areas[1].get_parent().get_index()
-		
 		index += 1
 
 	card.reparent(cards_holder)
 	cards_holder.move_child(card, index)
 
-
 func _on_button_pressed() -> void:
-	print("cards drawed")
-	var cardpos
-	var cardneg
-	cardvalueselector(cardpos,cardneg)
+	print("Cards drawn")
 	
-	var card = cardResource.instantiate()
-	cards_holder.add_child(card)
-	set_new_card(card)
+	var cardpos: Array = []  #  Initialize cardpos to store positive card values.
+	var cardneg: Array = []  #  Initialize cardneg to store negative card values.
+	
+	var selected_values = cardvalueselector()  #  Call the selector function to get random card values.
+	cardpos = selected_values[0]  #  Assign the first value (positive card) to cardpos.
+	cardneg = selected_values[1]  #  Assign the second value (negative card) to cardneg.
+	
+	var card = cardResource.instantiate()  # Ahmad: Create an instance of the card resource.
+	cards_holder.add_child(card)  # Ahmad: Add the new card instance to the cards holder.
+	set_new_card(card)  # Ahmad: Call set_new_card to reposition and set the home field for the new card.
 
-	
-	
-	
-	
-
-func cardvalueselector(positive_value,negative_value):
-	positive_value = card_positive[randf_range(0,card_positive.size())]
-	negative_value = card_negative[randf_range(0,card_negative.size())]
-	print(positive_value)
-	print(negative_value)
+func cardvalueselector() -> Array:  # Ahmad: Changed the return type to Array for better data handling.
+	var positive_value = card_positive[randi() % card_positive.size()]  #  Randomly select a positive card value.
+	var negative_value = card_negative[randi() % card_negative.size()]  #  Randomly select a negative card value.
+	print(positive_value)  #  Print the selected positive value for debugging purposes.
+	print(negative_value)  #  Print the selected negative value for debugging purposes.
+	return [positive_value, negative_value]  # Return both values as an array to simplify value retrieval.
